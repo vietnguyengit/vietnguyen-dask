37de5226759a90a7f9153e2ef3bf5766d742f3aa
Viet Nguyen <vh.nguyen@utas.edu.au>
2022-08-02 16:04:42 +1000

    Update ec2-deploy.sh

--- ec2-deploy.sh
+++ ec2-deploy.sh
@@ -31,7 +31,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo apt-get install -y docker-compose
 sudo chmod 666 /var/run/docker.sock
